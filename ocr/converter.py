f80fa644946b5f63d73dc12e10cdec7a1353dffb
hyun <juseng12@naver.com>
2023-01-31 03:25:50 +0900

    converter

---
+++ ocr/converter.py
@@ -0,0 +1,32 @@
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class LabelConverter(object):
+    def __init__(self, character):
+        list_token = ['[GO]', '[s]']  
+        # list_character = list(character)
+        self.character = list_token + character
+
+        self.dict = {}
+        for i, char in enumerate(self.character):
+            # print(i, char)
+            self.dict[char] = i
+
+    def encode(self, text, batch_max_length=5):
+        length = [len(s) + 1 for s in text]
+        batch_max_length += 1
+        batch_text = torch.LongTensor(len(text), batch_max_length + 1).fill_(0)
+        for i, t in enumerate(text):
+            text = list(t)
+            text.append('[s]')
+            text = [self.dict[char] for char in text]
+            batch_text[i][1:1 + len(text)] = torch.LongTensor(text)  # batch_text[:, 0] = [GO] token
+        return (batch_text.to(device), torch.IntTensor(length).to(device))
+
+    def decode(self, text_index, length):
+        texts = []
+        for index, l in enumerate(length):
+            text = ''.join([self.character[i] for i in text_index[index, :]])
+            texts.append(text)
+        return texts
